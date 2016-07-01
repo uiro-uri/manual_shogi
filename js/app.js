58f8862a94e512c0dcc1d624dff740829c202ad6
M uiro <ticktackbohn@gmail.com>
2016-07-01 07:02:25 +0000

    rotate opossite tokens

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 function init() {
     var board = document.createElement("table");
+    var cellsize = 40;
+    var promotionarea = 3;
     document.body.appendChild(board);
     
     var boardcontents = [
@@ -13,23 +15,31 @@ function init() {
         ["","角","","","","","","飛",""],
         ["香","桂","銀","金","玉","金","銀","桂","香"]
     ];
-    var dragedobj;
-    
+
     for(var i = 0;i < boardcontents.length;++i){
         var tr = board.insertRow(-1);
 		for(var j = 0;j < boardcontents[i].length;++j){
             var td = tr.insertCell(-1);
-            var element = document.createElement("div");
-            element.className = "token";
-            if (boardcontents[i][j].length != 0){
-                element.appendChild(document.createTextNode(boardcontents[i][j]));
+            var cell = document.createElement("div");
+            cell.className = "cell";
+            var token = document.createElement("div");
+            token.className = "token";
+            if(i<promotionarea){
+                token.style.transform = "rotate(180deg)";
             }
-            td.appendChild(element);
-            td.height = 20;
+            token.style.fontSize = cellsize * 0.8;
+            if (boardcontents[i][j].length != 0){
+                token.appendChild(document.createTextNode(boardcontents[i][j]));
+            }
+            td.appendChild(cell);
+            cell.appendChild(token);
+            td.height = cellsize;
+            td.width = cellsize;
 		}
 	}
+	
     $(function(){
-        $(".token").draggable();
+        $(".token").draggable({cursor:"move"});
     });
 	board.rules = "all";
 	board.frame = "box";
